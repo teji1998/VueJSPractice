90dfe9c5bb92786f61b161b27e8686690628ef27
teji1998 <tejasvinirpk@gmail.com>
2022-04-24 03:45:49 +0530

    Refactored : Code to understand templates and data

--- Vue-FireBase/app.js
+++ Vue-FireBase/app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     //data,functions
-    //template : '<h2>Hi, I am the template !</h2>'
+    data() {
+        return {
+            title : 'The Final Empire',
+            author : 'Brandon Sanderson',
+            age : 45
+        }
+
+    }
 })
 
 app.mount('#app')
