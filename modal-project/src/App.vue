3dda582e1b31c3e145c62a62ecb53ca3c9c21f30
teji1998 <tejasvinirpk@gmail.com>
2022-05-03 19:18:44 +0530

    Added:Data function to display heading

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Welcome to my first Vue App</h1>
+  <h1>{{title}}</h1>
 </template>
 
 <script>
@@ -7,8 +7,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      title : 'Welcome to my first Vue App !!!'
+    }
+    
   }
 }
 </script>
