69ecf062ddd761076acc2acd655087986f8e9b42
teji1998 <tejasvinirpk@gmail.com>
2022-05-03 16:57:05 +0530

    Added:Code for dynamic classes

--- Vue-FireBase/app.js
+++ Vue-FireBase/app.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
             url : 'http://www.thenetninja.co.uk',
             showBooks : true,
             books :[
-                {title : 'Name of the wind', author: 'Patrick Rothfuss',img: 'assets/NameOfWind.jpg'},
-                {title :'The way of Kings', author : 'Brandon Sanderson',img: 'assets/WayOfKings.jpg'},
-                {title :'The final Empire', author : 'Brandon Sanderson',img: 'assets/FinalEmpire.jpg'},
+                {title : 'Name of the wind', author: 'Patrick Rothfuss',img: 'assets/NameOfWind.jpg',isFav:true},
+                {title :'The way of Kings', author : 'Brandon Sanderson',img: 'assets/WayOfKings.jpg',isFav:false},
+                {title :'The final Empire', author : 'Brandon Sanderson',img: 'assets/FinalEmpire.jpg',isFav:true},
             ]
         }
 
